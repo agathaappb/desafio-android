f2b9dff85c2cbfff23ee8cb89ed1d3fe827693c4
agathaappb <agatha.alana@hotmail.com>
2023-08-06 20:51:56 -0300

    feat: commit ViewModel

--- app/src/main/java/com/picpay/desafio/android/presenter/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/picpay/desafio/android/presenter/viewmodel/UserViewModel.kt
@@ -1,39 +1,29 @@
 package com.picpay.desafio.android.presenter.viewmodel
 
-import android.app.Application
 import android.content.Context
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.picpay.desafio.android.UserListener
+import com.picpay.desafio.android.data.UserListener
 import com.picpay.desafio.android.data.model.User
-import com.picpay.desafio.android.data.repository.LocalRepository
-import com.picpay.desafio.android.data.repository.RemoteRepository
+import com.picpay.desafio.android.presenter.repository.LocalRepository
+import com.picpay.desafio.android.presenter.repository.RemoteRepository
+import com.picpay.desafio.android.usecase.UserUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class UserViewModel(): ViewModel() {
-
-
-
-    private val remoteRepository = RemoteRepository()
+@HiltViewModel
+class UserViewModel @Inject constructor(val useCase: UserUseCase): ViewModel() {
 
 
     private val _responseUsers = MutableLiveData<List<User>>()
     val responseUsers: LiveData<List<User>> = _responseUsers
 
-    fun getResponseUser(context: Context){
+    fun getResponseUser(){
 
-        remoteRepository.getListUsers(object : UserListener{
-            val localRepository = LocalRepository(context)
-            override fun onSucessResponse(response: List<User>) {
-                _responseUsers.value = response
-                localRepository.insert(response)
-            }
+        useCase.verifyCache()
 
-            override fun onFailureResponse(message: String) {
-                TODO("Not yet implemented")
-            }
+        _responseUsers.value = useCase.userData()
 
-        })
     }
 }
